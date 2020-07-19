5cb8eb5a8717d1a1676fe4dd12605287e7a26d3d
TPeschel <tpeschel@imise.uni-leipzig.de>
2020-07-19 03:03:01 +0200

    Add fhir_search, fhir_table

--- core.py
+++ core.py
@@ -1,93 +1,112 @@
 import xml.etree.ElementTree as ET
 import pandas as PA
-#import xml.parsers.expat as EP
+# import xml.parsers.expat as EP
 import re as RE
 import os
+import requests as REQ
 from typing import Dict, Any
+
+
+def which(x, y):
+    f = [z == y for z in x]
+    i = 0
+    for z in x:
+        if z == y:
+            return i
+            break
+        i += 1
+
+    return None
+
+
+def get_bundle(req):
+    bundle = REQ.get(req)
+    bundle_str = str(bundle.content, encoding="utf-8")
+    if bundle_str:
+        bundle_str = RE.sub('\s*xmlns\s*=\s*"[^"]+\s*"', '', bundle_str)
+        return ET.fromstring(bundle_str)
+    return None
 
 
 def load_bundle(path: str):
     bundle_str = open(path, "r", encoding='utf-8')
     if bundle_str:
-        bundle_str =bundle_str.read()
+        bundle_str = bundle_str.read()
         bundle_str = RE.sub('\s*xmlns\s*=\s*"[^"]+\s*"', '', bundle_str)
         return ET.fromstring(bundle_str)
-    return ""
+    return None
+
 
 def bundle2df(bundle, design_df):
-    #ns: Dict[Any, str] = dict(f="http://hl7.org/fhir")
-
     xpath_resource = design_df[0]
     table_design = design_df[1]
 
     xml_res = bundle.findall(xpath_resource)
 
-    df = [[0 for x in range(len(table_design.keys()))] for y in range(len(xml_res))]
+    if 0 < len(xml_res):
 
-    row = 0
-    for r in xml_res:
-        col = 0
-        for col_name in table_design.keys():
-            #xp = RE.sub("/@[-\w]+$", "", table_design[col_name])
-            xp = table_design[col_name]
-            y = r.findall(xp)
-            z = [x.attrib['value'] if 'value' in x.attrib else 'NA' for x in y]
-            df[row][col] = ' '.join(z)
-            col += 1
-        row += 1
+        df = [[0 for x in range(len(table_design.keys()))] for y in range(len(xml_res))]
 
-    df = PA.DataFrame(df)
-    df.columns = [str(k) for k in table_design.keys()]
+        row = 0
+        for r in xml_res:
+            col = 0
+            for col_name in table_design.keys():
+                # xp = RE.sub("/@[-\w]+$", "", table_design[col_name])
+                xp = table_design[col_name]
+                y = r.findall(xp)
+                z = [x.attrib['value'] if 'value' in x.attrib else 'NA' for x in y]
+                df[row][col] = ' '.join(z)
+                col += 1
+            row += 1
 
-    return df
+        df = PA.DataFrame(df)
+        df.columns = [str(k) for k in table_design.keys()]
+
+        return df
+
+    return None
 
 
-def main():
-    design = {
-        "Observations": (
-            ".//Observation",
-            {
-                "O.OID": "./id",
-                "O.PID": "./subject/reference",
-                "O.EID": "./encounter/reference",
-                "DIA": "./component/code/coding/code[@value='8462-4']/../../../valueQuantity/value",
-                "DIA.SYSTEM": "./component/code/coding/code[@value='8462-4']/../system",
-                "SYS": "./component/code/coding/code[@value='8480-6']/../../../valueQuantity/value",
-                "SYS.SYSTEM": "./component/code/coding/code[@value='8480-6']/../system",
-                "DATE": "./effectiveDateTime"
-            }
-        ),
-        "Encounters": (
-            ".//Encounter",
-            {
-                "E.EID": "id",
-                "E.PID": "subject/reference",
-                "START": "period/start",
-                "END": "period/end"
-            }
-        ),
-        "Patients": (
-            ".//Patient",
-            {
-                "P.PID": "id",
-                "GVN.NAME": "name/given",
-                "FAM.NAME": "name/family",
-                "SEX": "gender",
-                "DOB": "birthDate"
-            }
-        )
-    }
+def fhir_search(req):
+    bundles = []
 
-    print(os.getcwd())
+    r_cnt = 0
 
-    bundle = load_bundle('bundles/observations_with_patients_and_encounters_hapi.xml')
+    while req:
+        r_cnt += 1
+        print(str(r_cnt) + " : " + req)
+        bundle = get_bundle(req=req)
+        req = None
+        if bundle:
+            bundles.append(bundle)
+        r = bundle.findall(".//link/relation")
 
-    dfs = dict([(k, bundle2df(bundle, design[k])) for k in design.keys()])
+        if r:
+            u = bundle.findall(".//link/url")
+            s = [x.attrib['value'] for x in r]
+            if 0 < len(s):
+                n = which(s, 'next')
+                if n:
+                    req = u[n].attrib['value']
 
-    print(dfs)
+    return bundles
 
+
+def fhir_table(bundles, design):
+    dfs = [[bundle2df(bundle, design[k]) for bundle in bundles] for k in design.keys()]
+
+    dfs = dict([(d[1], PA.concat(d[0])) for d in zip(dfs, design.keys())])
+
+    return dfs
+
+
+"""
     if not('csv' in os.listdir(".")):
         os.mkdir("csv")
 
-    for k in dfs.keys():
-        dfs[k].to_csv("csv/"+k+'.csv', sep=';', decimal='.')
+    i = 0
+    for d in dfs:
+        for k in d.keys():
+            d[k].to_csv("csv/"+k+str(i)+'.csv', sep=';', decimal='.')
+        i += 1
+"""
--- main.py
+++ main.py
@@ -1,4 +1,60 @@
-import core
+import core as co
+import os
 
 if __name__ == '__main__':
-    core.main()
+
+    endp = "https://hapi.fhir.org/baseR4/"
+#    endp = "https://vonk.fire.ly/R4/"
+    req  = "Observation?code=http://loinc.org|85354-9&_include=Observation:patient&_include=Observation:encounter&_format=xml&_pretty=true&_count=500"
+#    req = "Observation?_include=Observation:patient&_include=Observation:encounter&_format=xml&_pretty=true&_count=500"
+
+    fsq = endp + req
+
+    design = {
+        "Observations": (
+            ".//Observation",
+            {
+                "O.OID": "./id",
+                "O.PID": "./subject/reference",
+                "O.EID": "./encounter/reference",
+                "DIA.VALUE": "./component/code/coding/code[@value='8462-4']/../../../valueQuantity/value",
+                "DIA.UNIT": "./component/code/coding/code[@value='8462-4']/../../../valueQuantity/unit",
+                "DIA.SYSTEM": "./component/code/coding/code[@value='8462-4']/../system",
+                "SYS.VALUE": "./component/code/coding/code[@value='8480-6']/../../../valueQuantity/value",
+                "SYS.UNIT": "./component/code/coding/code[@value='8480-6']/../../../valueQuantity/unit",
+                "SYS.SYSTEM": "./component/code/coding/code[@value='8480-6']/../system",
+                "DATE": "./effectiveDateTime"
+            }
+        ),
+        "Encounters": (
+            ".//Encounter",
+            {
+                "E.EID": "id",
+                "E.PID": "subject/reference",
+                "START": "period/start",
+                "END": "period/end"
+            }
+        ),
+        "Patients": (
+            ".//Patient",
+            {
+                "P.PID": "id",
+                "GVN.NAME": "name/given",
+                "FAM.NAME": "name/family",
+                "SEX": "gender",
+                "DOB": "birthDate"
+            }
+        )
+    }
+
+    bundles = co.fhir_search(fsq)
+
+    tables = co.fhir_table(bundles, design)
+
+    print(tables)
+
+    if not ('csv' in os.listdir(".")):
+        os.mkdir("csv")
+
+    for k in tables.keys():
+        tables[k].to_csv("csv/"+k+".csv", sep=";", decimal=".", encoding="utf-8")
