3c4a1d2d558f52d47431cb1fa9b6ae0c10ab84f0
Julian Brandes <julian.brandes@medizin.uni-leipzig.de>
2020-12-03 11:48:13 +0100

    minor changes

--- demo.py
+++ demo.py
@@ -10,8 +10,6 @@ df_med_codes = getMedicationCodes(dct_med, ";")
 mask_atc = df_med_codes["system"] == "http://www.dimdi.de/atcgm2018"
 print(df_med_codes[mask_atc])
 
-#df = dct_ing['Medications']
-#df = rm_indices(df)
 df_codes = getMedicationIngredientCodes(dct_ing, ";", onlyAPI=True)
 mask_cas = df_codes[0]["system"] == "http://www.cas.org/cas"
 print(df_codes[0][mask_cas])
--- extractMedCodes.py
+++ extractMedCodes.py
@@ -10,7 +10,7 @@ def getMedicationIngredientCodes(dct_ing, sep=";", onlyAPI=False):
         onlyAPI: if true -> returns only active pharmaceutical ingredients
         
     Returns:
-        list of Pandas dataframes, one dataframe for each ingredient: [ DataFrame(index|system|code) ]
+        list of Pandas dataframes, one dataframe for each ingredient: [ DataFrame(index|system|code|display) ]
     """
 
     df = dct_ing['Medications']
